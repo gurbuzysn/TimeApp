4293678769926cf4696f697db36fa80aa2671421
yasinn gürbüz <yasingurbuz1@hotmail.com>
2023-12-07 23:26:14 +0300

    Add project files.

---
+++ TimeApp/Controllers/TimeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TimeApp.Controllers
+{
+    public class TimeController : Controller
+    {
+        public IActionResult Time()
+        {
+            DateTime dateTime = DateTime.Now;
+            return View(dateTime);
+        }
+    }
+}
